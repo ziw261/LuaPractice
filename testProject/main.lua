c0ba3514d415fd020050dbf22e4de507cbd5174f
ziw261 <ziw261@ucsd.edu>
2020-10-14 23:29:15 -0400

    Update main.lua

--- testProject/main.lua
+++ testProject/main.lua
@@ -1,3 +1,18 @@
+message = 6
+chicken = 10
+output = chicken + message * chicken
+
+condition = false
+
+if condition == true then
+    output = 0.4
+end
+
+if output >=  1.9 then
+    output = output + 2.3
+end
+
 function love.draw()
-    love.graphics.print("Hello World!")
+    love.graphics.setFont(love.graphics.newFont(50))
+    love.graphics.print(output)
 end
