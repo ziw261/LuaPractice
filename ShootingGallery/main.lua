1e656eac864f8c4c79e4b0cbcf9737c47bd6ebd7
ziw261 <ziw261@ucsd.edu>
2020-10-17 17:33:58 -0400

    Lua wtih first project setted up

---
+++ ShootingGallery/main.lua
@@ -0,0 +1,10 @@
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+    love.graphics.rectangle("fill", 200, 400, 200, 100)
+    love.graphics.circle("line", 400, 200, 100)
+end
